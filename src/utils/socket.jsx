ac4837d615ec03ad53c13193512cb15acabd9e31
Harjot Tathgur <tathgurharjot8@gmail.com>
2022-11-25 01:04:54 -0500

    1

--- src/utils/socket.jsx
+++ src/utils/socket.jsx
@@ -2,7 +2,7 @@ import React, { createContext, useContext } from "react";
 
 import { io } from "socket.io-client";
 
-const serverUrl = "http://localhost:4000";
+const serverUrl = "http://167.99.183.90:4000";
 
 const socket = io(serverUrl);
 
